dfa24008b1fcf6cbd6286eb2f3c9caf0381198cb
gamelolo <saytanpotong@gmail.com>
2017-01-12 08:10:50 +0700

    Initial commit

---
+++ app/src/main/java/kku/photong/saitan/iret/MainApplication.java
@@ -0,0 +1,22 @@
+package kku.photong.saitan.iret;
+
+import android.app.Application;
+
+/**
+ * Created by Game on 11-Jan-17.
+ */
+
+public class MainApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Initialze
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+    }
+}
---
+++ app/src/main/java/kku/photong/saitan/iret/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package kku.photong.saitan.iret.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import kku.photong.saitan.iret.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
